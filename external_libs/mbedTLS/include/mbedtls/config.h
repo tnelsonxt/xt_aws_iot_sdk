ff4f08e342350cb796bb891ad3a6c4c3e1e2fe81
Tim Nelson <tnelson@xirgotech.com>
2017-02-27 14:18:08 -0600

    disable Counter Block Cipher mode (CTR) for symmetric ciphers.

--- external_libs/mbedTLS/include/mbedtls/config.h
+++ external_libs/mbedTLS/include/mbedtls/config.h
@@ -328,7 +328,7 @@
  *
  * Enable Counter Block Cipher mode (CTR) for symmetric ciphers.
  */
-#define MBEDTLS_CIPHER_MODE_CTR
+//#define MBEDTLS_CIPHER_MODE_CTR
 
 /**
  * \def MBEDTLS_CIPHER_NULL_CIPHER
