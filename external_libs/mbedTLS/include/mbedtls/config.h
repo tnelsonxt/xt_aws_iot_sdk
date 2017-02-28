1831233561c798b48bd5ad78a4fd254145371fa6
Tim Nelson <tnelson@xirgotech.com>
2017-02-28 01:32:23 -0600

    disable support for x.509 certificate creation

--- external_libs/mbedTLS/include/mbedtls/config.h
+++ external_libs/mbedTLS/include/mbedtls/config.h
@@ -2306,7 +2306,7 @@
  *
  * This module is the basis for creating X.509 certificates and CSRs.
  */
-#define MBEDTLS_X509_CREATE_C
+//#define MBEDTLS_X509_CREATE_C
 
 /**
  * \def MBEDTLS_X509_CRT_WRITE_C
@@ -2319,7 +2319,7 @@
  *
  * This module is required for X.509 certificate creation.
  */
-#define MBEDTLS_X509_CRT_WRITE_C
+//#define MBEDTLS_X509_CRT_WRITE_C
 
 /**
  * \def MBEDTLS_X509_CSR_WRITE_C
@@ -2332,7 +2332,7 @@
  *
  * This module is required for X.509 certificate request writing.
  */
-#define MBEDTLS_X509_CSR_WRITE_C
+//#define MBEDTLS_X509_CSR_WRITE_C
 
 /**
  * \def MBEDTLS_XTEA_C
